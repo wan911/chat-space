cad877777e37d4e7f33869ed18ee72ff0a49d6f1
minori <minori.ap.wan@gmail.com>
2019-09-30 15:10:27 +0900

    Add group creation view file

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,19 @@
 class GroupsController < ApplicationController
+  def new
+  end
+
+  def create(params_group)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def params_group
+    params.reqire(:group).permit(:name)
+  end
 end
