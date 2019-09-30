7683a78717a7ccb8730aff544b680a69f69bc7c0
minori <minori.ap.wan@gmail.com>
2019-09-30 14:19:42 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'chats#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :chats, only: [:index, :create] 
+  end
 end
