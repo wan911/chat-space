dba2fbda581f78fcc738cf71b14835667c6d27d0
minori <minori.ap.wan@gmail.com>
2019-10-01 00:18:24 +0900

    Fix routing 'chats#index' -> 'groups#index'
    Fix routing 'chats#index' -> 'groups#index'
    Move view sidebar to partial template

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'chats#index'
+  root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update] do
+  resources :groups, only: [:new, :create, :edit, :update] do
     resources :chats, only: [:index, :create] 
   end
 end
