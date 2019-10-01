ca20edc1d285b5a7966310956a934df2c2a2da21
minori <minori.ap.wan@gmail.com>
2019-10-01 22:01:34 +0900

    Add message sending function

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,8 +1,29 @@
 class ChatsController < ApplicationController
+  before_action :set_group
+
   def index
+    @chat  = Chat.new
+    @chats = @group.chats.includes(:user)
   end
 
   def create
+    @chat = @group.chats.new(chat_params)
+    if @chat.save
+      redirect_to group_chats_path(@group), notice: 'メッセージが送信されました'
+    else
+      @chats = @group.chats.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください'
+      render :index
+    end
   end
 
+  private
+
+  def chat_params
+    params.require(:chat).permit(:message, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,6 @@
 class Chat < ApplicationRecord
   validates :message, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
+  belongs_to :group
+  belongs_to :user
 end
