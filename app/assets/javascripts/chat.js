5ed0cb1a52c70a0d737454c8afa43a885f238dc6
minori <minori.ap.wan@gmail.com>
2019-10-08 19:41:12 +0900

    Change the contents of fail
    Change from console log to alert.
    Change content of alert comment.

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -61,7 +61,7 @@ $(function(){
         })
       })
       .fail(function(){
-        console.log('error');
+        alert('自動更新に失敗しました');
       });
     };
     setInterval(reloadMessages, 5000);
