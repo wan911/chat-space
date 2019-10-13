eddeca27e3d11b6e68004663f8eb2c98f7b8943a
minori <minori.ap.wan@gmail.com>
2019-10-13 19:36:59 +0900

    Add Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :rbenv_ruby, '2.5.1'
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/ChatSpace.pem']
 set :unicorn_pid, -> {"#{shared_path}/tmp/pids/unicorn.pid"}
+set :unicorn_config_path, -> {"#{current_path}/config/unicorn.rb"}
 set :keep_releases, 5
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
